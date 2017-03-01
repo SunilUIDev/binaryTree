fc144f34beca90aba409e7c902ea99080f2065d9
Sunilkumar Singh <sunilkumars@CESCHN.COM>
2017-03-01 22:16:34 +0530

    configuration changes

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.set('port', (process.env.PORT || 8200));
 app.use(express.static(__dirname + '/app'));
 
 app.listen(app.get('port'), function () {
-    console.log('app is running on port', app.get('port'));
+    console.log('binary tree app is running on port', app.get('port'));
 });
